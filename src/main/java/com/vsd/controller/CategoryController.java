4f20091d2b7c88a148280f242cc599efec557699
Vinayak Deshmukh <vinayakd9623@gmail.com>
2026-08-03 19:00:17 +0530

    modify category api

--- src/main/java/com/vsd/controller/CategoryController.java
+++ src/main/java/com/vsd/controller/CategoryController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/v1/category")
+@RequestMapping("/api/v1/category")
 public class CategoryController {
 
     private final CategoryService categoryService;
